a29b1bd71e2ba27beca04733c749b088d550f381
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-14 01:07:25 +0200

    create basic auth empty  class and mange  it in the app code

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Define a class to manage the API authentication using Basic Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ For the moment this class will be empty.
+
+    Args:
+        Auth (_type_): _description_
+    """
