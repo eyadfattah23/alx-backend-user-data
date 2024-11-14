7cf4b3a47ee45d7fa2d8c18f6230285f735b1d6e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-15 01:59:28 +0200

    change the order of errors

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,11 +40,12 @@ def before_request():
                                              '/api/v1/auth_session/login/'])):
         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if not auth.authorization_header(request) and not auth.session_cookie(request):
+    if not auth.authorization_header(request) \
+            and not auth.session_cookie(request):
+        abort(401)
+    if auth.authorization_header(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
 
