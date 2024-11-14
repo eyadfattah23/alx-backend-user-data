54c092ca0d1974c1a73765d1c2ad8ac320e41a1e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-14 22:55:14 +0200

    Improve  require_auth(self, path, excluded_paths) by allowing * at the end of excluded paths.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@
 from flask import request
 from typing import List, TypeVar
 from models.user import User
+import re
 
 
 class Auth:
@@ -20,27 +21,43 @@ class Auth:
 
         Returns:
             bool:
-                True if:
-                    * the path is not in the list of strings
-                    * path is None
-                    * excluded_paths is None or empty
-                    * excluded_paths is None or empty
-                otherwise False
+            True:
+                    the path is not in the list of strings
+                    path is None
+                    excluded_paths is None or empty
+                    excluded_paths is None or empty
+            False:
+                    otherwise
+
+        Example for excluded_paths = ["/api/v1/stat*"]:
+
+            /api/v1/users will return True
+            /api/v1/status will return False
+            /api/v1/stats will return False
+
         """
 
-        """ if (path is None) \
+        if (path is None) \
                 or (excluded_paths is None) \
                 or (excluded_paths == []):
             return True
 
         if (path in excluded_paths) or (path+"/" in excluded_paths):
             return False
+
         else:
-            return True """
-        return (path is None or excluded_paths is None or excluded_paths == []
+            for ex_path in excluded_paths:
+                if '*' in ex_path:
+                    actual_path = ex_path.replace('*', '.+')
+                    if re.search(actual_path, ex_path):
+                        return False
+
+            return True
+        """ return ((path is None) \
+            or (excluded_paths is None) or (excluded_paths == [])
                 or
-                path not in excluded_paths
-                and path + "/" not in excluded_paths)
+                (path not in excluded_paths
+                and path + "/" not in excluded_paths)) """
 
     def authorization_header(self, request=None) -> str:
         """check if the request has the required authorization header
