4be2a65739ed5876318819a7af98a6412eca7e0b
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-15 02:11:53 +0200

    handle if the there's a cookie and no headers case

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,8 +43,6 @@ def before_request():
     if not auth.authorization_header(request) \
             and not auth.session_cookie(request):
         abort(401)
-    if auth.authorization_header(request) is None:
-        abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
