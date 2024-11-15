50dcf9c34c714dcca54f756fe71769d2ddf88482
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-15 02:10:29 +0200

    revert order again

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -40,13 +40,13 @@ def before_request():
                                              '/api/v1/auth_session/login/'])):
         return
 
-    if auth.current_user(request) is None:
-        abort(403)
     if not auth.authorization_header(request) \
             and not auth.session_cookie(request):
         abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
     request.current_user = auth.current_user(request)
 
 
