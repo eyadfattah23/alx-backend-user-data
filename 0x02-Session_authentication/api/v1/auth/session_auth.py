5db7d667c416fcf2140f1e27519f2a77bf5ab8c7
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-15 02:19:09 +0200

    Create an instance method def current_user(self, request=None): (overload) that returns a User instance based on a cookie value:

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@
 
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -49,3 +50,14 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value:
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+        """
+
+        usr_id = self.user_id_for_session_id(self.session_cookie(request))
+
+        return User.get(usr_id)
