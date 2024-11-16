93129493556064755451ce3249b86444a0743aa5
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-17 01:35:37 +0200

    pep8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -43,7 +43,8 @@ def user_login() -> str:
     return jsonify({"error": "wrong password"}), 401
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def user_logout() -> str:
     """ deletes the user session / logout
     """
