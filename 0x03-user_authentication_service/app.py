39bbdbb502c1a3456f2b374c0c0b062ed217d91d
eyadfattah23 <eyadabdelfattah4@gmail.com>
2024-11-24 06:38:16 +0200

    implement a login function to respond to the POST /sessions route.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """simple module for starting a flask server"""
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def hello_world():
 
 @app.route("/users", methods=["POST"])
 def users():
-    """end-point to register a user"""
+    """end-point to register a new user"""
     AUTH = Auth()
 
     email, password = request.form.get('email'), request.form.get('password')
@@ -27,6 +27,31 @@ def users():
         return jsonify({'email': usr.email, 'message': "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """create a new session for the user, store it the session ID as a cookie
+    """
+    AUTH = Auth()
+
+    email, password = request.form.get('email'), request.form.get('password')
+    if not email or not password:
+        return jsonify({"message": "missing parameters"}), 400
+
+    IsValidLogin = AUTH.valid_login(email, password)
+
+    if not IsValidLogin:
+        abort(401)
+    sessionID = AUTH.create_session(email)
+
+    if not sessionID:
+        abort(401)
+
+    resp = jsonify({"email": "<user email>", "message": "logged in"})
+    resp.set_cookie("sessionID", sessionID)
+
+    return resp
 
 
 if __name__ == "__main__":
